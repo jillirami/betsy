6090248b8495b72e74aae70edd7283f2e2349780
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-01 09:25:33 -0700

    relationship between product and category updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :merchant 
-  has_many :categories 
   has_many :reviews 
   has_many :orderitems
+  has_and_belongs_to_many :categories 
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
