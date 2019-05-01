cf50812dc385dceaf7d392f0c2675d3b57aa682f
MyriamWD <myriam.waldend@gmail.com>
2019-05-01 16:01:43 -0700

    merchant model method auth hash added

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,12 @@ class Merchant < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def self.build_from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.uid = auth_hash[:uid]
+    merchant.provider = "github"
+    merchant.username = auth_hash["info"]["username"]
+    merchant.email = auth_hash["info"]["email"]
+  end
 end
