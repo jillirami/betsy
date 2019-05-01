f4394ea88ee8ccf07c60f5e4e4673923eb5b1f3a
MyriamWD <myriam.waldend@gmail.com>
2019-05-01 16:09:53 -0700

    merchant model authhash updated for username

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,7 +8,8 @@ class Merchant < ApplicationRecord
     merchant = Merchant.new
     merchant.uid = auth_hash[:uid]
     merchant.provider = "github"
-    merchant.username = auth_hash["info"]["username"]
+    merchant.name = auth_hash["info"]["name"]
+    merchant.username = auth_hash["info"]["nickname"]
     merchant.email = auth_hash["info"]["email"]
   end
 end
