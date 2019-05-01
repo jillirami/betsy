65e37a172411f81ce287c932f5dd3d4c5fa57844
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-01 13:57:28 -0700

    migration for adding productid to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_205323) do
+ActiveRecord::Schema.define(version: 2019_05_01_205616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 2019_05_01_205323) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
+  add_foreign_key "reviews", "products"
 end
