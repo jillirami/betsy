74ce9a6353d561b31003f74372553d562bcf419f
Ngoc Le <ngocle@outlook.com>
2019-05-02 17:13:39 -0700

    edited gitignore file and added new and create methods in products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,4 +13,39 @@ class ProductsController < ApplicationController
       redirect_to products_path
     end
   end
+
+  def new
+    @product = Product.new
+
+    if params[:merchant_id]
+      @product.merchant = Merchant.find_by(id: params[:merchant_id])
+    end
+  end
+
+  def create
+    product = Product.new(product_params)
+
+    is_successful = product.save
+
+    if is_successful
+      flash[:success] = "Product added successfully"
+      redirect_to product_path(product.id)
+    else
+      product.errors.messages.each do |field, messages|
+        flash.now[field] = messages
+      end
+
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def find_individual_product
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def product_params
+    return params.require(:product).permit(:name, :price, :description, :photo, :inventory)
+  end
 end
