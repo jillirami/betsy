88f63efc42c2c160fed30c6b477c495aa01979ae
Ngoc Le <ngocle@outlook.com>
2019-05-03 15:59:58 -0700

    debugged

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,12 @@
 class ProductsController < ApplicationController
-  # before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
+  before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_product, only: [:show, :edit, :update, :retire]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find_by(id: params[:id])
-
     if @product.nil?
       flash[:error] = "Unknown product"
 
@@ -34,7 +34,7 @@ class ProductsController < ApplicationController
       redirect_to product_path(product.id)
     else
       product.errors.messages.each do |field, messages|
-        flash.now[field] = messages
+        flash.now[:error_form] = messages
       end
 
       render :new, status: :bad_request
@@ -42,10 +42,8 @@ class ProductsController < ApplicationController
   end
 
   def retired
-    product = Product.find_by(id: params[:id])
-
-    product.toggle(:retired)
-    product.save
+    @product.toggle(:retired)
+    @product.save
 
     #is there another page we would want to fallback to? check test if we change.
     redirect_back(fallback_location: products_path)
@@ -53,7 +51,7 @@ class ProductsController < ApplicationController
 
   private
 
-  def find_individual_product
+  def find_product
     @product = Product.find_by(id: params[:id])
   end
 
