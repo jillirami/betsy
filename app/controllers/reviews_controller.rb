c91f22e6c1f37b3abc768e9c3712e4e8b4aeb233
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-03 17:02:01 -0700

    attempt at review controller and view, potential route added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,32 @@
 class ReviewsController < ApplicationController
-  def index
+  # def index
+  # end
+
+  def new
+    @review = Review.new #(description: "Default Review", product_id: params[:product_id])
+
+    if params[:product_id]
+      @review.product_id = Product.find_by(id: params[:product_id]).id
+    end
+  end
+
+  def create
+    review = Review.new(review_params)
+
+    if review.save
+      flash[:success] = "Successfully created review!"
+      redirect_to root_path #product_reviews_path(product.id) #review_path(product.params[:product_id])
+    else
+      flash[:failure] = "Could not create review"
+      flash[:messages] = review.errors.messages
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def review_params
+    return params.require(:review).permit(:rating, :description, :product_id)
   end
 end
+#
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   resources :merchants
   resources :orders
 
+  # resources :merchants do
+    # resources :reviews, only: [:new, :create]
+  # end
+
+
   get "/merchants/current", to: "merchants#current", as: "current_merchant"
   get "/merchants/dashboard/:id", to: "merchants#dashboard", as: "dashboard"
   get "/auth/github", as: "github_login"
