877f0d77d8e9d4e0edceacb7fa377b62a6e6315f
MyriamWD <myriam.waldend@gmail.com>
2019-05-07 11:43:23 -0700

    added test for browse products  by category

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,14 @@ describe Product do
   it "must be valid" do
     value(product).must_be :valid?
   end
+
+  it "browses products by category" do
+    category = categories(:cat1)
+    product = Product.new(category_ids: [category.id])
+
+    browse_result = Product.browse_by_category(category)
+
+    expect(browse_result).must_be_kind_of ActiveRecord::Relation
+    expect(product.category_ids).must_include category.id
+  end
 end
