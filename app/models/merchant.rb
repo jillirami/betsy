63319801f2dcf4d1b024f38e79a7c56ad721de6f
MyriamWD <myriam.waldend@gmail.com>
2019-05-07 23:17:01 -0700

    added find products by merchant, find orderitems by merchant, calculate subtotal by orderitem

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,4 +13,28 @@ class Merchant < ApplicationRecord
     merchant.email = auth_hash["info"]["email"]
     return merchant
   end
+
+  def self.find_products(merchant)
+    Product.where(merchant_id: merchant.id)
+  end
+
+  def self.find_order_items(merchant)
+    orders = []
+
+    products = find_products(merchant)
+    products.each do |p|
+      order_items = Orderitem.find_order_items_by_product(p)
+      order_items.each do |order_items|
+        orders << order_items
+      end
+    end
+    return orders.sort!
+  end
+
+  def self.calculate_subtotal(order_item)
+    subtotal_cents = 0
+    subtotal_cents += order_item.quantity * order_item.product.price
+    subtotal = subtotal_cents / 100.00
+    return subtotal
+  end
 end
