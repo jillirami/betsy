d4b4ff66529e4efb065ae1d7661bc099dd10cdf3
Ngoc Le <ngocle@outlook.com>
2019-05-08 14:08:45 -0700

    Migration: added reviewer column in the reviews model to store reviewer name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_234449) do
+ActiveRecord::Schema.define(version: 2019_05_08_205943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2019_05_04_234449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "product_id"
+    t.string "reviewer"
     t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
