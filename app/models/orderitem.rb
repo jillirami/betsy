83e75b6ae6c7bf0071bcd80ca7a590a8c6a92aa7
MyriamWD <myriam.waldend@gmail.com>
2019-05-07 23:15:43 -0700

    added find orderitems by product method

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -2,5 +2,10 @@ class Orderitem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: {greater_than: 0}
+
+  def self.find_order_items_by_product(product)
+    where(product_id: product.id)
+  end
+  
 end
