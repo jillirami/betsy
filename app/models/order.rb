ed896cf1c885a54103ce9bb952a570be4c6f27e7
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-08 10:20:42 -0700

    corrected cart function to prevent duplicate orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,12 +4,8 @@ class Order < ApplicationRecord
   def cart
     cart = {}
 
-    total_quantity = 0
-
     self.orderitems.each do |order_item|
-      total_quantity += order_item.quantity
-
-      cart[Product.find_by(id: order_item.product_id).name] = total_quantity
+      cart[order_item] = 0 + order_item.quantity
     end
     return cart
   end
