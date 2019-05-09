562adf1a9eac597af6f18cf887d47d0ca21d288f
MyriamWD <myriam.waldend@gmail.com>
2019-05-08 23:49:27 -0700

    created display_shipment method for toggle flavor

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -2,7 +2,7 @@ class Orderitem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: {greater_than: 0}
 
   def subtotal
     return self.quantity * (Product.find_by(id: self.product_id).price / 100)
@@ -11,5 +11,12 @@ class Orderitem < ApplicationRecord
   def self.find_order_items_by_product(product)
     where(product_id: product.id)
   end
-  
+
+  def self.display_shipment(order_item)
+    if order_item.status?
+      return "Yes"
+    else
+      return "Not yet"
+    end
+  end
 end
