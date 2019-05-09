3fc8441f23a363e2a375c836a25c212d1dc659fd
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-09 15:58:53 -0700

    test to prevent order item creation if logged in

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,9 +1,47 @@
 require "test_helper"
 
 describe OrderItemsController do
-  it "should get index" do
-    get order_items_index_url
-    value(response).must_be :success?
+  describe "create" do
+    it "will not create an order item if logged in as merchant" do
+      merchant = merchants(:jewelry)
+
+      perform_login(merchant)
+
+      post order_items_path
+
+      expect(flash[:error]).must_equal "Log out of your merchant account to shop!"
+      must_respond_with :redirect
+      must_redirect_to products_path
+    end
+
+    # it "will create an instance of an order if none already present" do
+    #   session[:order]
+
+            # Orderitem.create(quantity: 2, product_id: products(:two), order_id: orders(:order1))
+    # end
+
+  #   it "will not create an instance of an order if already present" do
+  #   end
+
+  #   it "can create an order item" do
+  #   end
+
+  #   it "reduced inventory of product when order created" do
+  #   end
+
+  #   it "will not create an order item if not enough inventory present" do
+  #   end
   end
 
+  # describe "edit" do
+  # end
+
+  # describe "update" do
+  # end
+
+  # describe "destroy" do
+  # end
+
+  # describe "random_create" do
+  # end
 end
