5b71fde2aab7dc4b79c424adf521073b88ad6cb4
Ngoc Le <ngocle@outlook.com>
2019-05-09 17:32:34 -0700

    added custom method test

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -30,7 +30,7 @@ describe Review do
   describe "relationships" do
     it "belongs to a product" do
       #product fixture :two has been assigned a review in the review fixture file
-      expect(product.reviews.first.rating).must_equal review.rating
+      expect(product.reviews[0].rating).must_equal review.rating
     end
 
     it "can set the product through the product_id" do
@@ -49,6 +49,22 @@ describe Review do
       product.reviews << review
 
       expect(product.reviews).must_include review
+    end
+  end
+  describe "average rating - custom method" do
+    it "returns 0 if the product has no rating" do
+      product_with_no_rating = products(:one)
+
+      average_rating = Review.average_rating(product_with_no_rating)
+
+      expect(average_rating).must_equal 0
+    end
+
+    it "returns the accurate average if the product has more than 1 rating" do
+      # product has two ratings of 5 and 1 (see review fixture). The average rating expected is 3 [(5+1)/2]
+      average_rating = Review.average_rating(product)
+
+      expect(average_rating).must_equal 3
     end
   end
 end
