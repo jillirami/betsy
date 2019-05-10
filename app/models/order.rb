dad50ed8834928358c3889f293e77c504cce0924
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-09 17:48:50 -0700

    variable names ammended

--- app/models/order.rb
+++ app/models/order.rb
@@ -28,15 +28,15 @@ class Order < ApplicationRecord
     return cart_total
   end
 
-  def items_pending
+  def shipped_items
     if self.status == "paid"
-      shipped_orders = 0
+      shipped_items = 0
       self.orderitems.each do |order_item|
         if order_item.status == true
-          shipped_orders += 1
+          shipped_items += 1
         end
       end
-      return shipped_orders
+      return shipped_items
     end
   end
 end
